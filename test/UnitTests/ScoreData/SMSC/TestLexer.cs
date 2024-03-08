01276be666d40263a247820331fbd0a985cfd35a
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2024-03-09 01:13:21 +0900

    :+1: Add tests for lexer and parser

---
+++ test/UnitTests/ScoreData/SMSC/TestLexer.cs
@@ -0,0 +1,95 @@
+﻿using SoundMaker.ScoreData.SMSC;
+
+namespace SoundMakerTests.UnitTests.ScoreData.SMSC;
+
+public class TestLexer
+{
+    [Fact(DisplayName = "コメントは解析しないか")]
+    public void TestReadAll_Comment()
+    {
+        var data = @"// Comment";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        Assert.Empty(tokens);
+    }
+
+    [Fact(DisplayName = "通常の音符を解析できるか")]
+    public void TestReadAll_Note()
+    {
+        var data = @"// Note
+C4, 16
+";
+        var lexer = new Lexer(data);
+        var expected = new List<Token>()
+        {
+            new(TokenType.Alphabet, "C", 2),
+            new(TokenType.Number, "4", 2),
+            new(TokenType.Comma, ",", 2),
+            new(TokenType.Number, "16", 2),
+        };
+        var tokens = lexer.ReadAll();
+        Assert.Equal(expected.Count, tokens.Count);
+
+        AssertToken(expected[0], tokens[0]);
+        AssertToken(expected[1], tokens[1]);
+        AssertToken(expected[2], tokens[2]);
+        AssertToken(expected[3], tokens[3]);
+    }
+
+    [Fact(DisplayName = "付点音符を解析できるか")]
+    public void TestReadAll_DottedNote()
+    {
+        var data = @"// Note
+C4, 16.
+";
+        var lexer = new Lexer(data);
+        var expected = new List<Token>()
+        {
+            new(TokenType.Alphabet, "C", 2),
+            new(TokenType.Number, "4", 2),
+            new(TokenType.Comma, ",", 2),
+            new(TokenType.Number, "16", 2),
+            new(TokenType.Dot, ".", 2),
+        };
+        var tokens = lexer.ReadAll();
+        Assert.Equal(expected.Count, tokens.Count);
+
+        AssertToken(expected[0], tokens[0]);
+        AssertToken(expected[1], tokens[1]);
+        AssertToken(expected[2], tokens[2]);
+        AssertToken(expected[3], tokens[3]);
+        AssertToken(expected[4], tokens[4]);
+    }
+
+    [Fact(DisplayName = "半音上の音符を解析できるか")]
+    public void TestReadAll_SharpNote()
+    {
+        var data = @"// Note
+C#4, 16
+";
+        var lexer = new Lexer(data);
+        var expected = new List<Token>()
+        {
+            new(TokenType.Alphabet, "C", 2),
+            new(TokenType.Sharp, "#", 2),
+            new(TokenType.Number, "4", 2),
+            new(TokenType.Comma, ",", 2),
+            new(TokenType.Number, "16", 2),
+        };
+        var tokens = lexer.ReadAll();
+        Assert.Equal(expected.Count, tokens.Count);
+
+        AssertToken(expected[0], tokens[0]);
+        AssertToken(expected[1], tokens[1]);
+        AssertToken(expected[2], tokens[2]);
+        AssertToken(expected[3], tokens[3]);
+        AssertToken(expected[4], tokens[4]);
+    }
+
+    private void AssertToken(Token expected, Token actual)
+    {
+        Assert.Equal(expected.Type, actual.Type);
+        Assert.Equal(expected.Literal, actual.Literal);
+        Assert.Equal(expected.LineNumber, actual.LineNumber);
+    }
+}
---
+++ test/UnitTests/ScoreData/SMSC/TestParser.cs
@@ -0,0 +1,201 @@
+﻿using SoundMaker.ScoreData.SMSC;
+using SoundMaker.Sounds.Score;
+
+namespace SoundMakerTests.UnitTests.ScoreData.SMSC;
+
+public class TestParser
+{
+    [Fact(DisplayName = "普通の音符を解析できるか")]
+    public void TestParse_Note()
+    {
+        var data = @"C4, 16";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+
+        var expectedCollection = new List<ISoundComponent>()
+        {
+            new Note(Scale.C, 4, LengthType.Sixteenth),
+        };
+        var actualCollection = parser.Parse();
+        Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+        var expected = (Note)expectedCollection[0];
+        var actual = Assert.IsType<Note>(actualCollection[0]);
+        Assert.Equal(expected.Scale, actual.Scale);
+        Assert.Equal(expected.ScaleNumber, actual.ScaleNumber);
+        Assert.Equal(expected.Length, actual.Length);
+        Assert.Equal(expected.IsDotted, actual.IsDotted);
+    }
+
+    [Fact(DisplayName = "付点音符を解析できるか")]
+    public void TestParse_DottedNote()
+    {
+        var data = @"C4, 16.";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+
+        var expectedCollection = new List<ISoundComponent>()
+        {
+            new Note(Scale.C, 4, LengthType.Sixteenth, true),
+        };
+        var actualCollection = parser.Parse();
+        Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+        var expected = (Note)expectedCollection[0];
+        var actual = Assert.IsType<Note>(actualCollection[0]);
+        Assert.Equal(expected.Scale, actual.Scale);
+        Assert.Equal(expected.ScaleNumber, actual.ScaleNumber);
+        Assert.Equal(expected.Length, actual.Length);
+        Assert.Equal(expected.IsDotted, actual.IsDotted);
+    }
+
+    [Fact(DisplayName = "半音上の音符を解析できるか")]
+    public void TestParse_SharpNote()
+    {
+        var data = @"C#4, 16";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+
+        var expectedCollection = new List<ISoundComponent>()
+        {
+            new Note(Scale.CSharp, 4, LengthType.Sixteenth),
+        };
+        var actualCollection = parser.Parse();
+        Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+        var expected = (Note)expectedCollection[0];
+        var actual = Assert.IsType<Note>(actualCollection[0]);
+        Assert.Equal(expected.Scale, actual.Scale);
+        Assert.Equal(expected.ScaleNumber, actual.ScaleNumber);
+        Assert.Equal(expected.Length, actual.Length);
+        Assert.Equal(expected.IsDotted, actual.IsDotted);
+    }
+
+    [Fact(DisplayName = "普通の休符を解析できるか")]
+    public void TestParse_Rest()
+    {
+        var data = @"rest, 16";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+
+        var expectedCollection = new List<ISoundComponent>()
+        {
+            new Rest(LengthType.Sixteenth),
+        };
+        var actualCollection = parser.Parse();
+        Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+        var expected = (Rest)expectedCollection[0];
+        var actual = Assert.IsType<Rest>(actualCollection[0]);
+        Assert.Equal(expected.Length, actual.Length);
+        Assert.Equal(expected.IsDotted, actual.IsDotted);
+    }
+
+    [Fact(DisplayName = "付点休符を解析できるか")]
+    public void TestParse_DottedRest()
+    {
+        var data = @"rest, 16.";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+
+        var expectedCollection = new List<ISoundComponent>()
+        {
+            new Rest(LengthType.Sixteenth, true),
+        };
+        var actualCollection = parser.Parse();
+        Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+        var expected = (Rest)expectedCollection[0];
+        var actual = Assert.IsType<Rest>(actualCollection[0]);
+        Assert.Equal(expected.Length, actual.Length);
+        Assert.Equal(expected.IsDotted, actual.IsDotted);
+    }
+
+    [Fact(DisplayName = "タイを解析できるか")]
+    public void TestParse_Tie()
+    {
+        var data = @"tie(C#4, 16, 8, 8, 8.)";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+
+        var expectedCollection = new List<ISoundComponent>()
+        {
+            new Tie(new(Scale.CSharp, 4, LengthType.Sixteenth), new List<Note>()
+            {
+                new(LengthType.Eighth),
+                new(LengthType.Eighth),
+                new(LengthType.Eighth, true),
+            }),
+        };
+        var actualCollection = parser.Parse();
+        Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+        var expected = (Tie)expectedCollection[0];
+        var actual = Assert.IsType<Tie>(actualCollection[0]);
+        Assert.Equal(expected.Count, actual.Count);
+    }
+
+    [Fact(DisplayName = "連符を解析できるか")]
+    public void TestParse_Tuplet()
+    {
+        var data = @"tup(16, C#4, rest, E4.)";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+
+        var expectedCollection = new List<ISoundComponent>()
+        {
+            new Tuplet(new List<ISoundComponent>()
+            {
+                new Note(Scale.CSharp, 4, LengthType.Whole),
+                new Rest(LengthType.Eighth),
+                new Note(Scale.E, 4,LengthType.Whole, true),
+            }, LengthType.Sixteenth),
+        };
+        var actualCollection = parser.Parse();
+        Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+        var expected = (Tuplet)expectedCollection[0];
+        var actual = Assert.IsType<Tuplet>(actualCollection[0]);
+        Assert.Equal(expected.Count, actual.Count);
+    }
+
+    [Fact(DisplayName = "連符内のタイや連符を解析できるか")]
+    public void TestParse_TupletInTupletAndTie()
+    {
+        var data = @"tup(16, tup(16, rest, rest), tie(C#4, 16, 8, 8, 8.))";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+
+        var rest = new Rest(LengthType.Eighth);
+        var expectedCollection = new List<ISoundComponent>()
+        {
+            new Tuplet(new List<ISoundComponent>()
+            {
+                new Tuplet(new List<ISoundComponent>()
+                {
+                    rest, rest
+                }, LengthType.Sixteenth),
+                new Tie(new(Scale.CSharp, 4, LengthType.Sixteenth), new List<Note>()
+                {
+                    new(LengthType.Eighth),
+                    new(LengthType.Eighth),
+                    new(LengthType.Eighth, true),
+                }),
+            }, LengthType.Sixteenth),
+        };
+        var actualCollection = parser.Parse();
+        Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+        var expected = (Tuplet)expectedCollection[0];
+        var actual = Assert.IsType<Tuplet>(actualCollection[0]);
+        Assert.Equal(expected.Count, actual.Count);
+    }
+}
