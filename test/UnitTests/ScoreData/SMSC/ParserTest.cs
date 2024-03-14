0c5acbf8524c27f60ca83f7b18765dfc68c3d11a
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2024-03-14 19:05:27 +0900

    :+1: Add tests of errors.

--- test/UnitTests/ScoreData/SMSC/ParserTest.cs
+++ test/UnitTests/ScoreData/SMSC/ParserTest.cs
@@ -130,7 +130,7 @@ public class ParserTest
     [Fact(DisplayName = "タイを解析できるか")]
     public void TestParse_Tie()
     {
-        var data = @"tie(C#4, 16, 8, 8, 8.)";
+        var data = @"tie(C#4, 16, 8.)";
         var lexer = new Lexer(data);
         var tokens = lexer.ReadAll();
         var parser = new Parser(tokens);
@@ -139,8 +139,6 @@ public class ParserTest
         {
             new Tie(new(Scale.CSharp, 4, LengthType.Sixteenth), new List<Note>()
             {
-                new(LengthType.Eighth),
-                new(LengthType.Eighth),
                 new(LengthType.Eighth, true),
             }),
         };
@@ -214,5 +212,75 @@ public class ParserTest
         var expected = (Tuplet)expectedCollection[0];
         var actual = Assert.IsType<Tuplet>(actualCollection[0]);
         Assert.Equal(expected.Count, actual.Count);
+    }
+
+    // ------------------エラーのテスト---------------------------
+
+    [Fact(DisplayName = "空の文でエラーが発生しないか")]
+    public void TestEmptyStatment()
+    {
+        var data = ";;;";
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+        var actual = parser.Parse().Errors;
+        Assert.Empty(actual);
+    }
+
+    [Theory(DisplayName = "音程解析エラーのテスト")]
+    [InlineData("C100,1\n D8,1 // A0～C8のみ許可する", 2)]
+    [InlineData("A0,1; C8,1", 0)]
+    public void Test_ScaleErrors(string data, int expectedCount)
+    {
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+        var actual = parser.Parse().Errors.Count(err => err.Type is SMSCReadErrorType.InvalidScale);
+        Assert.Equal(expectedCount, actual);
+    }
+
+    [Theory(DisplayName = "長さ解析エラーのテスト")]
+    [InlineData("C4,1; C4,2; C4,4; C4,8; C4,16; C4,32; C4,1; C4,64", 0)]
+    [InlineData("C4,0; C4,65; C4,", 3)]
+    public void Test_LengthErrors(string data, int expectedCount)
+    {
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+        var actual = parser.Parse().Errors.Count(err => err.Type is SMSCReadErrorType.InvalidLength);
+        Assert.Equal(expectedCount, actual);
+    }
+
+    [Theory(DisplayName = "カンマが見つからないエラーのテスト")]
+    [InlineData("C4.1; tie(C4.1)", 2)]
+    public void Test_NotFoundCommaErrors(string data, int expectedCount)
+    {
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+        var actual = parser.Parse().Errors.Count(err => err.Type is SMSCReadErrorType.NotFoundComma);
+        Assert.Equal(expectedCount, actual);
+    }
+
+    [Theory(DisplayName = ")が見つからないエラーのテスト")]
+    [InlineData("tie(C4,1;tup(4.,C4.", 2)]
+    public void TestNotFoundRightParenthesesErrors(string data, int expectedCount)
+    {
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+        var actual = parser.Parse().Errors.Count(err => err.Type is SMSCReadErrorType.NotFoundRightParentheses);
+        Assert.Equal(expectedCount, actual);
+    }
+
+    [Theory(DisplayName = "(が見つからないエラーのテスト")]
+    [InlineData("tie)C4,1;tup4.,C4.", 2)]
+    public void TestNotFoundLeftParenthesesErrors(string data, int expectedCount)
+    {
+        var lexer = new Lexer(data);
+        var tokens = lexer.ReadAll();
+        var parser = new Parser(tokens);
+        var actual = parser.Parse().Errors.Count(err => err.Type is SMSCReadErrorType.NotFoundLeftParentheses);
+        Assert.Equal(expectedCount, actual);
     }
 }
